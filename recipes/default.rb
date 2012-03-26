324498a8f532bfc6f1edc18e9da074f1c31c1452
Andrew Crump <andrew@kotirisoftware.com>
2012-03-26 19:24:02 +0100

    FC023: Prefer conditional attributes.

--- recipes/default.rb
+++ recipes/default.rb
@@ -84,13 +84,12 @@ execute "create #{node['wordpress']['db']['database']} database" do
 end
 
 # save node data after writing the MYSQL root password, so that a failed chef-client run that gets this far doesn't cause an unknown password to get applied to the box without being saved in the node data.
-unless Chef::Config[:solo]
-  ruby_block "save node data" do
-    block do
-      node.save
-    end
-    action :create
+ruby_block "save node data" do
+  block do
+    node.save
   end
+  not_if { Chef::Config[:solo] }
+  action :create
 end
 
 log "Navigate to 'http://#{server_fqdn}/wp-admin/install.php' to complete wordpress installation" do
